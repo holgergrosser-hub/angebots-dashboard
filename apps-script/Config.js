bf937536c54d5268306c06784531467a37269160
Holger Grosser <holger.grosser@iso9001.info>
2026-02-03 06:08:08 +0100

    Add inputUrl and new sheet config

--- apps-script/Config.js
+++ apps-script/Config.js
@@ -28,13 +28,28 @@ const CONFIG = {
       sheetName: 'Anfragen',
       spreadsheetId: '1jNY1BmHNC-iZK-l7DwSmx6m6rsfPvr6kcXGtvjyparE',
       tabName: 'Anfragen',
-      inputUrl: '',
+      inputUrl: 'https://qm-guru-angebote-9001.netlify.app/',
       offerUrl: 'https://docs.google.com/spreadsheets/d/1jNY1BmHNC-iZK-l7DwSmx6m6rsfPvr6kcXGtvjyparE/edit',
       columns: {
         DATUM: 'A',
         KUNDENNAME: 'B',
         BETRAG: 'I',
         STATUS: 'M'
+      },
+      aktiv: true
+    },
+    {
+      sheetId: 'qm-system-angebot',
+      sheetName: 'QM Sytem Angebot',
+      spreadsheetId: '1yrY6xPz34UH2PtESTPanxfMNxas6mtkpvmodg882p8U',
+      tabName: 'Tabellenblatt1',
+      inputUrl: 'https://qm-guru-angebote-9001.netlify.app/',
+      offerUrl: 'https://docs.google.com/spreadsheets/d/1yrY6xPz34UH2PtESTPanxfMNxas6mtkpvmodg882p8U/edit?gid=0#gid=0',
+      columns: {
+        DATUM: 'C',
+        KUNDENNAME: 'D',
+        BETRAG: 'U',
+        STATUS: 'Z'
       },
       aktiv: true
     }
